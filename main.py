3756ef84c9c30e54fff108d0df7c2a3dd926478f
Autumn ✮⋆˙ <autumnleigh2020@gmail.com>
2026-03-18 20:36:50 -0400

    *main.py

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ import loaded_dictionary
 
 word_list = loaded_dictionary.load('2of4brif.txt')
 print(word_list)
+
+# Create a list for anagrams
+anagrams = []
+
+# Ask user for a word
+user_word = input("Enter a word: ").lower()
+
+# Sort the user word
+sorted_user_word = sorted(user_word)
+
+# Loop through word list
+for word in word_list:
+  # Compare sorted words
+  if sorted(word) == sorted_user_word and word != user_word:
+    anagrams.append(word)
+
